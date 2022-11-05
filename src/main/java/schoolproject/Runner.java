269d862fdc4e22438be1eb799b0e5838021c3691
Betul Cavus <btlcvss@gmail.com>
2022-11-05 16:39:13 +0300

    second commit

---
+++ src/main/java/schoolproject/Runner.java
@@ -0,0 +1,15 @@
+package schoolproject;
+
+public class Runner {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        Ogretmen.feykOgretmenEkle();
+        Ogrenci.feykOgrenciEkle();
+        Depo.anaMenu();
+
+
+
+    }
+
+}
